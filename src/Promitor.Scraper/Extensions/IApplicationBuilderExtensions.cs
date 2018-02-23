3b3df9e9654782712641eb0518bc243149972a44
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-02-23 15:14:15 +0100

    Add basic scrape endpoint with fixed endpoint (#12)

---
+++ src/Promitor.Scraper/Extensions/IApplicationBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Prometheus.Client.Owin;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class IApplicationBuilderExtensions
+    {
+        public static void UsePrometheusScraper(this IApplicationBuilder app)
+        {
+            var prometheusOptions = new PrometheusOptions
+            {
+                MapPath = "prometheus/scrape"
+            };
+
+            app.UsePrometheusServer(prometheusOptions);
+        }
+    }
+}
