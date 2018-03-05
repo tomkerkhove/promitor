3885ad3dfd77b04bb3cd15a2292f52d8c6a604bf
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-03-05 09:34:13 -0800

    Validate setup before running

--- src/Promitor.Scraper/Startup.cs
+++ src/Promitor.Scraper/Startup.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Promitor.Scraper.Configuration;
 using Promitor.Scraper.Configuration.Providers;
 using Promitor.Scraper.Configuration.Providers.Interfaces;
 using Promitor.Scraper.Scraping;
@@ -12,10 +15,9 @@ namespace Promitor.Scraper
     {
         public Startup(IHostingEnvironment env)
         {
-            var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.AddEnvironmentVariables();
+            ValidateSetup();
 
-            Configuration = configurationBuilder.Build();
+            Configuration = BuildConfiguration();
             ScrapeEndpointBasePath = ScrapeEndpoint.GetBasePath(Configuration);
         }
 
@@ -42,6 +44,25 @@ namespace Promitor.Scraper
             services.UseOpenApiSpecifications(ScrapeEndpointBasePath, apiVersion: 1);
 
             services.AddTransient<IScrapeConfigurationProvider, ScrapeConfigurationProvider>();
+        }
+
+        private IConfiguration BuildConfiguration()
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddEnvironmentVariables();
+
+            return configurationBuilder.Build();
+        }
+
+        private void ValidateSetup()
+        {
+            var configurationPath = Environment.GetEnvironmentVariable(EnvironmentVariables.ConfigurationPath);
+            if (File.Exists(configurationPath) == false)
+            {
+                var errorMessage = $"Scrape configuration at '{configurationPath}' does not exist";
+                Console.WriteLine(errorMessage);
+                throw new Exception(errorMessage);
+            }
         }
     }
 }
