59b83d13aacd25a7f9f7721715358c8efa2d36a9
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-03-07 14:53:04 -0800

    Add basic unit tests for validation steps

---
+++ src/Promitor.Scraper.Tests.Unit/Validation/ConfigurationPathValidationStepTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using Promitor.Scraper.Configuration;
+using Promitor.Scraper.Validation.Steps;
+using Xunit;
+
+namespace Promitor.Scraper.Tests.Unit.Validation
+{
+    [Category("Unit")]
+    public class ConfigurationPathValidationStepTests
+    {
+        [Fact]
+        public void ConfigurationPath_FileDoesNotExist_Succeeds()
+        {
+            // Arrange
+            const string validConfigurationPath = "Invalid";
+            Environment.SetEnvironmentVariable(EnvironmentVariables.ConfigurationPath, validConfigurationPath);
+
+            // Act
+            var scrapingScheduleValidationStep = new ConfigurationPathValidationStep();
+            var validationResult = scrapingScheduleValidationStep.Validate();
+
+            // Assert
+            Assert.False(validationResult.IsSuccessful);
+        }
+
+        [Fact]
+        public void ConfigurationPath_FileExists_Succeeds()
+        {
+            // Arrange
+            const string invalidConfigurationPath = "Files/valid-sample.yaml";
+            Environment.SetEnvironmentVariable(EnvironmentVariables.ConfigurationPath, invalidConfigurationPath);
+
+            // Act
+            var scrapingScheduleValidationStep = new ConfigurationPathValidationStep();
+            var validationResult = scrapingScheduleValidationStep.Validate();
+
+            // Assert
+            Assert.True(validationResult.IsSuccessful);
+        }
+    }
+}
