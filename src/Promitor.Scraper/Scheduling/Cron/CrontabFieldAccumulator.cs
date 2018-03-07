11b5de64b1e9fc0f3496f36ee80afc387ffd61b8
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-03-07 12:42:59 -0800

    Remove legacy

--- src/Promitor.Scraper/Scheduling/Cron/CrontabFieldAccumulator.cs
+++
@@ -1,8 +0,0 @@
-﻿/*
- * Based on example by Maarten Balliauw - https://blog.maartenballiauw.be/post/2017/08/01/building-a-scheduled-cache-updater-in-aspnet-core-2.html
- * Thank you!
- */
-namespace Promitor.Scraper.Scheduling.Cron
-{
-    public delegate void CrontabFieldAccumulator(int start, int end, int interval);
-}
