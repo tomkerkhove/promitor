c714aec9e38c7bd17a9c242f7ef544f3f6edeb1e
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-04-11 18:14:22 +0200

    Change metrics model

---
+++ src/Promitor.Scraper/Model/Configuration/AzureMetricConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Management.Monitor.Models;
+
+namespace Promitor.Scraper.Model.Configuration
+{
+    public class AzureMetricConfiguration
+    {
+        /// <summary>
+        ///     Type of aggregation to query the Azure Monitor metric
+        /// </summary>
+        public AggregationType Aggregation { get; set; }
+
+        /// <summary>
+        ///     Name of the Azure Monitor metric to query
+        /// </summary>
+        public string MetricName { get; set; }
+    }
+}
