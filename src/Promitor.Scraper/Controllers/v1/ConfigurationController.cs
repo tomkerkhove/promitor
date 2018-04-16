501f9d8ebd53fee0c2da7f87407edd4a8b565c60
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-04-16 23:03:25 +0200

    Assign default scraping cron + Optimize open api specs (#51)
    * Assign default scraping cron
    
    * Optimize open api specs

--- src/Promitor.Scraper/Controllers/v1/ConfigurationController.cs
+++ src/Promitor.Scraper/Controllers/v1/ConfigurationController.cs
@@ -10,23 +10,25 @@ namespace Promitor.Scraper.Controllers.v1
     [Route("api/v1/configuration")]
     public class ConfigurationController : Controller
     {
-        private readonly IScrapeConfigurationProvider scrapeConfigurationProvider;
+        private readonly IScrapeConfigurationProvider _scrapeConfigurationProvider;
 
         public ConfigurationController(IScrapeConfigurationProvider scrapeConfigurationProvider)
         {
-            this.scrapeConfigurationProvider = scrapeConfigurationProvider;
+            _scrapeConfigurationProvider = scrapeConfigurationProvider;
         }
 
         /// <summary>
-        ///     Provides a list of all configured metrics to scrape
+        ///     Get Metrics Declaration
         /// </summary>
+        /// <remarks>Provides a list of all configured metrics to scrape</remarks>
         [HttpGet]
-        [SwaggerOperation("Get Scrape Configuration")]
-        [SwaggerResponse((int) HttpStatusCode.OK, Description = "Configuration concerning the metrics to scrape", Type = typeof(List<MetricDefinition>))]
+        [SwaggerOperation("get-metrics-declaration")]
+        [SwaggerResponse((int) HttpStatusCode.OK, Description = "Configuration concerning the metrics to scrape",
+            Type = typeof(List<MetricDefinition>))]
         [SwaggerResponse((int) HttpStatusCode.NoContent, Description = "No configured metrics were found to scrape")]
         public IEnumerable<MetricDefinition> Get()
         {
-            var scrapeConfiguration = scrapeConfigurationProvider.GetConfiguration();
+            var scrapeConfiguration = _scrapeConfigurationProvider.GetConfiguration();
             return scrapeConfiguration.Metrics;
         }
     }
