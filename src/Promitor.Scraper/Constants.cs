a24427e7f408ef834bea6cc7f9ad3169d1927761
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-04-16 22:28:24 +0200

    Provide logging about validation results (#50)
    * Rough validation loggin
    
    * Cleanup
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Scraper/Constants.cs
+++ src/Promitor.Scraper/Constants.cs
@@ -10,6 +10,16 @@
 ██╔═══╝ ██╔══██╗██║   ██║██║╚██╔╝██║██║   ██║   ██║   ██║██╔══██╗
 ██║     ██║  ██║╚██████╔╝██║ ╚═╝ ██║██║   ██║   ╚██████╔╝██║  ██║
 ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝";
+
+            internal static class Validation
+            {
+                internal const string SeperationText = "-------------------------------------------------";
+                internal const string StartValidationText = "Starting validation of configuration...";
+                internal const string ValidationStepSuccessfulText = "\tValidation step succeeded.";
+                internal const string GeneralValidationFailed =
+                    "Configuration is invalid. Please fix them before running Promitor again";
+                internal const string GeneralValidationSucceeded = "Configuration is valid.";
+            }
         }
     }
 }
---
+++ src/Promitor.Scraper/Validation/Steps/ValidationStep.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Promitor.Scraper.Validation.Steps
+{
+    public class ValidationStep
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"\t\t{message}");
+        }
+    }
+}
