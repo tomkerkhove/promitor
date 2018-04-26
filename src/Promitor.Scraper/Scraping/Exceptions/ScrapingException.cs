301ca3ec7828b26e882ebdeb4849749366049584
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-04-26 17:58:43 +0200

    Add new scraping exception

---
+++ src/Promitor.Scraper/Scraping/Exceptions/ScrapingException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Promitor.Scraper.Scraping.Exceptions
+{
+    public class ScrapingException : Exception
+    {
+        public string MetricName { get; }
+
+        public ScrapingException(string metricName, string message) : base(message)
+        {
+            MetricName = metricName;
+        }
+    }
+}
