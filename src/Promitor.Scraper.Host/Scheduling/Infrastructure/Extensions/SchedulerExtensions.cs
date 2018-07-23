ead5832c920ac252187fffacce3d6c4efad85ceb
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-07-23 13:03:01 +0200

    Use built-in BackgroundService
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Scraper.Host/Scheduling/Infrastructure/Extensions/SchedulerExtensions.cs
+++ src/Promitor.Scraper.Host/Scheduling/Infrastructure/Extensions/SchedulerExtensions.cs
@@ -12,11 +12,6 @@ namespace Promitor.Scraper.Host.Scheduling.Infrastructure.Extensions
 {
     public static class SchedulerExtensions
     {
-        public static IServiceCollection AddScheduler(this IServiceCollection services)
-        {
-            return services.AddSingleton<IHostedService, SchedulerHostedService>();
-        }
-
         public static IServiceCollection AddScheduler(this IServiceCollection services, EventHandler<UnobservedTaskExceptionEventArgs> unobservedTaskExceptionHandler)
         {
             return services.AddSingleton<IHostedService, SchedulerHostedService>(serviceProvider =>
