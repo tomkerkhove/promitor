46a49b0440dcb36464849fbba40d7ed4d49279b2
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-02-06 17:12:31 +0100

    Provide capability to opt-out of metric timestamps (#291)
    Provide capability to opt-out of metric timestamps.
    
    Closes #290

---
+++ src/Promitor.Core/Infrastructure/FeatureFlag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Promitor.Core.Infrastructure
+{
+    public class FeatureFlag
+    {
+        /// <summary>
+        ///     Determine if a feature flag is active or not
+        /// </summary>
+        /// <param name="toggleName">Name of the feature flag</param>
+        /// <param name="defaultFlagState">Default state of the feature flag if it's not configured</param>
+        public static bool IsActive(string toggleName, bool defaultFlagState = true)
+        {
+            var environmentVariableName = $"PROMITOR_FEATURE_{toggleName}";
+            var rawFlag = Environment.GetEnvironmentVariable(environmentVariableName);
+
+            if (bool.TryParse(rawFlag, out var isActive))
+            {
+                return isActive;
+            }
+
+            return defaultFlagState;
+        }
+    }
+}
