f801fb57381f3564d34bf41a3016d7440baac26f
Julien Corioland <julien.corioland@live.fr>
2019-06-18 12:13:21 +0200

    add resource and scraper for Redis Cache

---
+++ src/Promitor.Core.Scraping/Configuration/Model/Metrics/ResourceTypes/CacheForRedisMetricDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace Promitor.Core.Scraping.Configuration.Model.Metrics.ResourceTypes
+{
+    public class CacheForRedisMetricDefinition : MetricDefinition
+    {
+        public string CacheName { get; set; }
+        public override ResourceType ResourceType { get; } = ResourceType.CacheForRedis;
+    }
+}
