a5a8b32d71b3ff33c01040167c1a3a323dc986b8
Julien Corioland <julien.corioland@live.fr>
2019-06-18 13:30:55 +0200

    add unit test for Cache for Redis serialization

---
+++ src/Promitor.Scraper.Tests.Unit/Serialization/MetricsDeclaration/MetricsDeclarationWithCacheForRedisYamlSerializationTests.cs
@@ -0,0 +1,77 @@
+﻿using Bogus;
+using Microsoft.Extensions.Logging.Abstractions;
+using Promitor.Core.Scraping.Configuration.Model.Metrics.ResourceTypes;
+using Promitor.Core.Scraping.Configuration.Serialization.Core;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Xunit;
+using MetricDefinition = Promitor.Core.Scraping.Configuration.Model.Metrics.MetricDefinition;
+
+namespace Promitor.Scraper.Tests.Unit.Serialization.MetricsDeclaration
+{
+    [Category("Unit")]
+    public class MetricsDeclarationWithCacheForRedisYamlSerializationTests : YamlSerializationTests<CacheForRedisMetricDefinition>
+    {
+        [Theory]
+        [InlineData("promitor1", @"* */1 * * * *", @"* */2 * * * *")]
+        [InlineData(null, null, null)]
+        public void YamlSerialization_SerializeAndDeserializeConfigForCacheForRedis_SucceedsWithIdenticalOutput(string resourceGroupName, string defaultScrapingInterval, string metricScrapingInterval)
+        {
+            // Arrange
+            var azureMetadata = GenerateBogusAzureMetadata();
+            var cacheForRedisMetricDefinition = GenerateBogusCacheForRedisMetricDefinition(resourceGroupName, metricScrapingInterval);
+            var metricDefaults = GenerateBogusMetricDefaults(defaultScrapingInterval);
+            var scrapingConfiguration = new Core.Scraping.Configuration.Model.MetricsDeclaration()
+            {
+                AzureMetadata = azureMetadata,
+                MetricDefaults = metricDefaults,
+                Metrics = new List<MetricDefinition>()
+                {
+                    cacheForRedisMetricDefinition
+                }
+            };
+            var configurationSerializer = new ConfigurationSerializer(NullLogger.Instance);
+
+            // Act
+            var serializedConfiguration = configurationSerializer.Serialize(scrapingConfiguration);
+            var deserializedConfiguration = configurationSerializer.Deserialize(serializedConfiguration);
+
+            // Assert
+            Assert.NotNull(deserializedConfiguration);
+            AssertAzureMetadata(deserializedConfiguration, azureMetadata);
+            AssertMetricDefaults(deserializedConfiguration, metricDefaults);
+            Assert.NotNull(deserializedConfiguration.Metrics);
+            Assert.Single(deserializedConfiguration.Metrics);
+            var deserializedMetricDefinition = deserializedConfiguration.Metrics.FirstOrDefault();
+            AssertMetricDefinition(deserializedMetricDefinition, cacheForRedisMetricDefinition);
+            var deserializedServiceBusMetricDefinition = deserializedMetricDefinition as CacheForRedisMetricDefinition;
+            AssertCacheForRedisMetricDefinition(deserializedServiceBusMetricDefinition, cacheForRedisMetricDefinition);
+        }
+
+        private static void AssertCacheForRedisMetricDefinition(CacheForRedisMetricDefinition deserializedServiceBusMetricDefinition, CacheForRedisMetricDefinition cacheForRedisMetricDefinition)
+        {
+            Assert.NotNull(deserializedServiceBusMetricDefinition);
+            Assert.Equal(cacheForRedisMetricDefinition.CacheName, deserializedServiceBusMetricDefinition.CacheName);
+        }
+
+        private CacheForRedisMetricDefinition GenerateBogusCacheForRedisMetricDefinition(string resourceGroupName, string metricScrapingInterval)
+        {
+            var bogusScrapingInterval = GenerateBogusScrapingInterval(metricScrapingInterval);
+            var bogusAzureMetricConfiguration = GenerateBogusAzureMetricConfiguration();
+
+            var bogusGenerator = new Faker<CacheForRedisMetricDefinition>()
+                .StrictMode(ensureRulesForAllProperties: true)
+                .RuleFor(metricDefinition => metricDefinition.Name, faker => faker.Lorem.Word())
+                .RuleFor(metricDefinition => metricDefinition.Description, faker => faker.Lorem.Sentence(wordCount: 6))
+                .RuleFor(metricDefinition => metricDefinition.ResourceType, faker => Core.Scraping.Configuration.Model.ResourceType.CacheForRedis)
+                .RuleFor(metricDefinition => metricDefinition.CacheName, faker => faker.Lorem.Word())
+                .RuleFor(metricDefinition => metricDefinition.AzureMetricConfiguration, faker => bogusAzureMetricConfiguration)
+                .RuleFor(metricDefinition => metricDefinition.ResourceGroupName, faker => resourceGroupName)
+                .RuleFor(metricDefinition => metricDefinition.Scraping, faker => bogusScrapingInterval)
+                .Ignore(metricDefinition => metricDefinition.ResourceGroupName);
+
+            return bogusGenerator.Generate();
+        }
+    }
+}
