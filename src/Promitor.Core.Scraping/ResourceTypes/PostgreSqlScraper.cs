c6d0610b40c192bcadc2dabc1bd48bc002fb0c36
Julien Corioland <jucoriol@microsoft.com>
2019-06-21 10:03:15 +0200

    add scraper definition for PostgreSql

---
+++ src/Promitor.Core.Scraping/ResourceTypes/PostgreSqlScraper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Management.Monitor.Fluent.Models;
+using Microsoft.Extensions.Logging;
+using Promitor.Core.Scraping.Configuration.Model;
+using Promitor.Core.Scraping.Configuration.Model.Metrics.ResourceTypes;
+using Promitor.Core.Telemetry.Interfaces;
+using Promitor.Integrations.AzureMonitor;
+using System;
+using System.Threading.Tasks;
+
+namespace Promitor.Core.Scraping.ResourceTypes
+{
+    public class PostgreSqlScraper : Scraper<PostgreSqlMetricDefinition>
+    {
+        private const string ResourceUriTemplate = "subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.DBforPostgreSQL/servers/{2}";
+
+        public PostgreSqlScraper(AzureMetadata azureMetadata, AzureMonitorClient azureMonitorClient, ILogger logger, IExceptionTracker exceptionTracker)
+           : base(azureMetadata, azureMonitorClient, logger, exceptionTracker)
+        {
+        }
+
+        protected override async Task<ScrapeResult> ScrapeResourceAsync(string subscriptionId, string resourceGroupName, PostgreSqlMetricDefinition metricDefinition, AggregationType aggregationType, TimeSpan aggregationInterval)
+        {
+            var resourceUri = string.Format(ResourceUriTemplate, subscriptionId, resourceGroupName, metricDefinition.ServerName);
+
+            var metricName = metricDefinition.AzureMetricConfiguration.MetricName;
+            var foundMetricValue = await AzureMonitorClient.QueryMetricAsync(metricName, aggregationType, aggregationInterval, resourceUri);
+
+            return new ScrapeResult(resourceUri, foundMetricValue);
+        }
+    }
+}
