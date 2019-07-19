d5cf7d1daa4ea6fcfce6092e44e80972bc7e422e
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-07-19 17:50:08 +0200

    Improve our current CI (#624)
    * Change CI build into multiple stages
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Run unit tests without building
    
    * Resharper
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Use build scripts
    
    * Provide transparency in result
    
    * Change CI build into multiple stages
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Run unit tests without building
    
    * Resharper
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Use build scripts
    
    * Provide transparency in result
    
    * Tweak
    
    * Lower correct stuff
    
    * Simplify transformation

---
+++ build/helm/CI/Transform-Chart.ps1
@@ -0,0 +1,32 @@
+Param(
+    [String]
+    [Parameter(Mandatory = $true)] $chartName = $(throw "Chart name is required"),
+    [String]
+    [Parameter(Mandatory = $true)] $imageName = $(throw "Image name is required"),
+    [String]
+    [Parameter(Mandatory = $true)] $imageTag = $(throw "Image tag is required")
+)
+
+echo 'Copying Chart folder'
+cp promitor-agent-scraper/ $chartName/ -r
+
+echo 'Determining image version'
+$imageVersion = "$imageTag".ToLower()
+echo 'Image version is $imageVersion'
+
+echo 'Changing name of chart'
+((Get-Content -path $chartName/Chart.yaml -Raw) -replace 'promitor-agent-scraper', $chartName) | Set-Content -Path $chartName/Chart.yaml
+
+echo 'Changing image tag'
+((Get-Content -path $chartName/values.yaml -Raw) -replace '1.0.0-preview-8', $imageVersion) | Set-Content -Path $chartName/values.yaml
+
+echo 'Changing repo name'
+((Get-Content -path $chartName/values.yaml -Raw) -replace 'tomkerkhove/promitor-agent-scraper', $imageName) | Set-Content -Path $chartName/values.yaml
+
+echo 'Change name of chart in documentation samples'
+((Get-Content -path $chartName/README.md -Raw) -replace 'promitor-agent-scraper', 'promitor-agent-scraper-ci') | Set-Content -Path $chartName/README.md
+
+echo 'Outputting transformed content'
+Get-Content -path $chartName/Chart.yaml -Raw
+Get-Content -path $chartName/values.yaml -Raw
+Get-Content -path $chartName/README.md -Raw
