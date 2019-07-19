6ab1058da4a334fcc8a6441c265f71e22c257cae
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-07-19 17:31:31 +0200

    Simplify transformation

--- build/helm/CI/Transform-Chart.ps1
+++ build/helm/CI/Transform-Chart.ps1
@@ -15,7 +15,7 @@ $imageVersion = "$imageTag".ToLower()
 echo 'Image version is $imageVersion'
 
 echo 'Changing name of chart'
-((Get-Content -path $chartName/Chart.yaml -Raw) -replace 'name: promitor-agent-scraper', 'name: $chartName') | Set-Content -Path $chartName/Chart.yaml
+((Get-Content -path $chartName/Chart.yaml -Raw) -replace 'promitor-agent-scraper', $chartName) | Set-Content -Path $chartName/Chart.yaml
 
 echo 'Changing image tag'
 ((Get-Content -path $chartName/values.yaml -Raw) -replace '1.0.0-preview-8', $imageVersion) | Set-Content -Path $chartName/values.yaml
@@ -27,5 +27,6 @@ echo 'Change name of chart in documentation samples'
 ((Get-Content -path $chartName/README.md -Raw) -replace 'promitor-agent-scraper', 'promitor-agent-scraper-ci') | Set-Content -Path $chartName/README.md
 
 echo 'Outputting transformed content'
+Get-Content -path $chartName/Chart.yaml -Raw
 Get-Content -path $chartName/values.yaml -Raw
 Get-Content -path $chartName/README.md -Raw
