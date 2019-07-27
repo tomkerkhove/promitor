f8decbe82fd029900514f4cbb771b8e6ea57c9f8
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-07-27 15:31:08 +0200

    Provide API endpoint for getting runtime configuration (#640)
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Scraper.Host/Controllers/v1/ConfigurationController.cs
+++ src/Promitor.Scraper.Host/Controllers/v1/ConfigurationController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Promitor.Core.Configuration.Model;
 using Promitor.Core.Scraping.Configuration.Model.Metrics;
 using Promitor.Core.Scraping.Configuration.Providers.Interfaces;
 using Swashbuckle.AspNetCore.Annotations;
@@ -10,10 +12,12 @@ namespace Promitor.Scraper.Host.Controllers.v1
     [Route("api/v1/configuration")]
     public class ConfigurationController : Controller
     {
+        private readonly IOptionsMonitor<RuntimeConfiguration> _runtimeConfiguration;
         private readonly IMetricsDeclarationProvider _metricsDeclarationProvider;
 
-        public ConfigurationController(IMetricsDeclarationProvider metricsDeclarationProvider)
+        public ConfigurationController(IOptionsMonitor<RuntimeConfiguration> runtimeConfiguration, IMetricsDeclarationProvider metricsDeclarationProvider)
         {
+            _runtimeConfiguration = runtimeConfiguration;
             _metricsDeclarationProvider = metricsDeclarationProvider;
         }
 
@@ -23,14 +27,28 @@ namespace Promitor.Scraper.Host.Controllers.v1
         /// <remarks>Provides a list of all configured metrics to scrape</remarks>
         [HttpGet]
         [Route("metric-declaration")]
-        [SwaggerOperation(OperationId = "Configuration_Get")]
-        [SwaggerResponse((int) HttpStatusCode.OK, Description = "Configuration concerning the metrics to scrape",
+        [SwaggerOperation(OperationId = "Configuration_GetMetricDeclaration")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Configuration concerning the metrics to scrape",
             Type = typeof(List<MetricDefinition>))]
-        [SwaggerResponse((int) HttpStatusCode.NoContent, Description = "No configured metrics were found to scrape")]
-        public IEnumerable<MetricDefinition> Get()
+        [SwaggerResponse((int)HttpStatusCode.NoContent, Description = "No configured metrics were found to scrape")]
+        public IActionResult GetMetricDeclaration()
         {
             var scrapeConfiguration = _metricsDeclarationProvider.Get(applyDefaults: true);
-            return scrapeConfiguration.Metrics;
+            return Ok(scrapeConfiguration.Metrics);
+        }
+
+        /// <summary>
+        ///     Get Runtime Configuration
+        /// </summary>
+        /// <remarks>Provides the runtime configuration</remarks>
+        [HttpGet]
+        [Route("runtime")]
+        [SwaggerOperation(OperationId = "Configuration_GetRuntime")]
+        [SwaggerResponse((int)HttpStatusCode.OK, Description = "Configuration concerning the runtime",
+            Type = typeof(RuntimeConfiguration))]
+        public IActionResult GetRuntime()
+        {
+            return Ok(_runtimeConfiguration.CurrentValue);
         }
     }
 }
