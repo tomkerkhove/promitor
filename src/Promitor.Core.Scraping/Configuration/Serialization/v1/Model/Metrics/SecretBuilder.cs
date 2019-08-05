74de816e5c840df4a72f652ba94866e071445308
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-05 16:01:39 +0100

    Remove GetSecretValue from builder
    I've removed the `GetSecretValue()` method from the v1 `SecretBuilder` object because it's only needed by the model object, not the serialization objects.

--- src/Promitor.Core.Scraping/Configuration/Serialization/v1/Model/Metrics/SecretBuilder.cs
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v1/Model/Metrics/SecretBuilder.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Promitor.Core.Scraping.Configuration.Model.Metrics;
+﻿using Promitor.Core.Scraping.Configuration.Model.Metrics;
 
 namespace Promitor.Core.Scraping.Configuration.Serialization.v1.Model.Metrics
 {
@@ -7,25 +6,6 @@ namespace Promitor.Core.Scraping.Configuration.Serialization.v1.Model.Metrics
     {
         public string RawValue { get; set; }
         public string EnvironmentVariable { get; set; }
-
-        /// <summary>
-        /// Provides the secret value based on the configured approach
-        /// </summary>
-        public string GetSecretValue()
-        {
-            if (string.IsNullOrWhiteSpace(EnvironmentVariable) == false)
-            {
-                var secretValue = Environment.GetEnvironmentVariable(EnvironmentVariable);
-                return secretValue;
-            }
-
-            if (string.IsNullOrWhiteSpace(RawValue) == false)
-            {
-                return RawValue;
-            }
-
-            return string.Empty;
-        }
 
         public Secret Build()
         {
