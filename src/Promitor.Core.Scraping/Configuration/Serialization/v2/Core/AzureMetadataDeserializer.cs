d0c39529b442f8dc09c485818dfbdf88abad719d
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-07 17:24:55 +0100

    Implement v2 AzureMetadataDeserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Core/AzureMetadataDeserializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using GuardNet;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model;
+using YamlDotNet.RepresentationModel;
+
+namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Core
+{
+    public class AzureMetadataDeserializer : IDeserializer<AzureMetadataV2>
+    {
+        private const string TenantIdTag = "tenantId";
+        private const string SubscriptionIdTag = "subscriptionId";
+        private const string ResourceGroupNameTag = "resourceGroupName";
+
+        public AzureMetadataV2 Deserialize(YamlNode node)
+        {
+            Guard.NotNull(node, nameof(node));
+
+            var mappingNode = node as YamlMappingNode;
+            if (mappingNode == null)
+            {
+                throw new ArgumentException(
+                    $"Expected a YamlMappingNode but received '{node.GetType()}'", nameof(node));
+            }
+
+            var metadata = new AzureMetadataV2();
+
+            if (mappingNode.Children.TryGetValue(TenantIdTag, out var tenantIdNode))
+            {
+                metadata.TenantId = tenantIdNode.ToString();
+            }
+
+            if (mappingNode.Children.TryGetValue(SubscriptionIdTag, out var subscriptionIdNode))
+            {
+                metadata.SubscriptionId = subscriptionIdNode.ToString();
+            }
+
+            if (mappingNode.Children.TryGetValue(ResourceGroupNameTag, out var resourceGroupNameNode))
+            {
+                metadata.ResourceGroupName = resourceGroupNameNode.ToString();
+            }
+            
+            return metadata;
+        }
+    }
+}
---
+++ src/Promitor.Scraper.Tests.Unit/Serialization/v2/Core/AzureMetadataDeserializerTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Core;
+using Xunit;
+
+namespace Promitor.Scraper.Tests.Unit.Serialization.v2.Core
+{
+    [Category("Unit")]
+    public class AzureMetadataDeserializerTests
+    {
+        private readonly AzureMetadataDeserializer _deserializer;
+
+        public AzureMetadataDeserializerTests()
+        {
+            _deserializer = new AzureMetadataDeserializer();
+        }
+
+        [Fact]
+        public void Deserialize_NodeNull_ThrowsException()
+        {
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => _deserializer.Deserialize(null));
+        }
+
+        [Fact]
+        public void Deserialize_NodeWrongType_ThrowsException()
+        {
+            // Arrange
+            var node = YamlUtils.CreateYamlNode("version: v1").Children["version"];
+
+            // Act / Assert
+            Assert.Throws<ArgumentException>(() => _deserializer.Deserialize(node));
+        }
+
+        [Fact]
+        public void Deserialize_Metadata_SetsFromYaml()
+        {
+            // Arrange
+            const string tenantId = "c8819874-9e56-4e3f-b1a8-1c0325138f27";
+            const string subscriptionId = "0f9d7fea-99e8-4768-8672-06a28514f77e";
+            const string resourceGroupName = "promitor";
+
+            var yamlText =
+$@"azureMetadata:
+    tenantId: '{tenantId}'
+    subscriptionId: '{subscriptionId}'
+    resourceGroupName: '{resourceGroupName}'";
+            var node = YamlUtils.CreateYamlNode(yamlText).Children["azureMetadata"];
+
+            // Act
+            var metadata = _deserializer.Deserialize(node);
+
+            // Assert
+            Assert.Equal(tenantId, metadata.TenantId);
+            Assert.Equal(subscriptionId, metadata.SubscriptionId);
+            Assert.Equal(resourceGroupName, metadata.ResourceGroupName);
+        }
+    }
+}
