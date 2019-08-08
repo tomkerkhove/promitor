99810c2906140a2a224b41e54df24e50dbdd4708
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-08 16:38:50 +0100

    Implement v2 cosmos db deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Model/ResourceTypes/CosmosDbResourceV2.cs
@@ -0,0 +1,10 @@
+﻿namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes
+{
+    public class CosmosDbResourceV2 : AzureResourceDefinitionV2
+    {
+        /// <summary>
+        /// The cosmos database name.
+        /// </summary>
+        public string DbName { get; set; }
+    }
+}
