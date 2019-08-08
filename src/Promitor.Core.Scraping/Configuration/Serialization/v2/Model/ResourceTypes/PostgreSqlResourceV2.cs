f22f4ae8d718d1eb57c7bbdab42c327d8808927e
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-08 17:24:52 +0100

    Implement v2 postgre deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Model/ResourceTypes/PostgreSqlResourceV2.cs
@@ -0,0 +1,10 @@
+﻿namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes
+{
+    public class PostgreSqlResourceV2 : AzureResourceDefinitionV2
+    {
+        /// <summary>
+        /// The postgre server name.
+        /// </summary>
+        public string ServerName { get; set; }
+    }
+}
