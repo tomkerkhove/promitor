7b69e4e18596305e00e2b1a098eca26481060c67
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-08 16:57:56 +0100

    Implement v2 generic resource deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Providers/GenericResourceDeserializer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes;
+using YamlDotNet.RepresentationModel;
+
+namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Providers
+{
+    public class GenericResourceDeserializer : ResourceDeserializer
+    {
+        private const string FilterTag = "filter";
+        private const string ResourceUriTag = "resourceUri";
+
+        public GenericResourceDeserializer(ILogger logger) : base(logger)
+        {
+        }
+
+        protected override AzureResourceDefinitionV2 DeserializeResource(YamlMappingNode node)
+        {
+            return new GenericResourceV2
+            {
+                Filter = GetString(node, FilterTag),
+                ResourceUri = GetString(node, ResourceUriTag)
+            };
+        }
+    }
+}
