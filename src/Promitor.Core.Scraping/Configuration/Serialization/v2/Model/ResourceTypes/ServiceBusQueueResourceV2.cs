986ebe946ffc618ca0224e75d9e29971724bf886
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-09 10:02:18 +0100

    Implemented v2 service bus queue deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Model/ResourceTypes/ServiceBusQueueResourceV2.cs
@@ -0,0 +1,15 @@
+﻿namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes
+{
+    public class ServiceBusQueueResourceV2 : AzureResourceDefinitionV2
+    {
+        /// <summary>
+        /// The service bus queue to scrape.
+        /// </summary>
+        public string QueueName { get; set; }
+
+        /// <summary>
+        /// The service bus namespace.
+        /// </summary>
+        public string Namespace { get; set; }
+    }
+}
