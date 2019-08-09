97aa006422de9527ea425541cc40dc6ab3337deb
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-09 10:28:18 +0100

    Implement v2 secret deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Core/SecretDeserializer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model;
+using YamlDotNet.RepresentationModel;
+
+namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Core
+{
+    public class SecretDeserializer : Deserializer<SecretV2>
+    {
+        private const string RawValueTag = "rawValue";
+        private const string EnvironmentVariableTag = "environmentVariable";
+
+        public SecretDeserializer(ILogger logger) : base(logger)
+        {
+        }
+
+        public override SecretV2 Deserialize(YamlMappingNode node)
+        {
+            var secret = new SecretV2
+            {
+                RawValue = GetString(node, RawValueTag),
+                EnvironmentVariable = GetString(node, EnvironmentVariableTag)
+            };
+
+            if (!string.IsNullOrEmpty(secret.RawValue) && !string.IsNullOrEmpty(secret.EnvironmentVariable))
+            {
+                Logger.LogWarning("Secret with environment variable '{EnvironmentVariable}' also has a rawValue provided.", secret.EnvironmentVariable);
+            }
+
+            return secret;
+        }
+    }
+}
---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Model/SecretV2.cs
@@ -0,0 +1,17 @@
+﻿namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Model
+{
+    public class SecretV2
+    {
+        /// <summary>
+        /// The value of the secret. If you don't want to put the secret value directly
+        /// in the metric configuration, use the <see cref="EnvironmentVariable"/> property
+        /// instead.
+        /// </summary>
+        public string RawValue { get; set; }
+
+        /// <summary>
+        /// The name of an environment variable to get the secret value from.
+        /// </summary>
+        public string EnvironmentVariable { get; set; }
+    }
+}
