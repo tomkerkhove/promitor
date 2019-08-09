6a0034ef32e783579ff0e1e6e3d73be5e293bb07
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-09 10:12:17 +0100

    Implement v2 virtual machine deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Providers/VirtualMachineDeserializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes;
+using YamlDotNet.RepresentationModel;
+
+namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Providers
+{
+    public class VirtualMachineDeserializer : ResourceDeserializer
+    {
+        private const string VirtualMachineNameTag = "virtualMachineName";
+
+        public VirtualMachineDeserializer(ILogger logger) : base(logger)
+        {
+        }
+
+        protected override AzureResourceDefinitionV2 DeserializeResource(YamlMappingNode node)
+        {
+            return new VirtualMachineResourceV2
+            {
+                VirtualMachineName = GetString(node, VirtualMachineNameTag)
+            };
+        }
+    }
+}
