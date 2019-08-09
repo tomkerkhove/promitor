daa63439549d2307706c64aed94706692abe2d6a
Adam Connelly <adam.rpconnelly@gmail.com>
2019-08-09 09:50:11 +0100

    Implement v2 redis cache deserializer

---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Model/ResourceTypes/RedisCacheResourceV2.cs
@@ -0,0 +1,10 @@
+﻿namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes
+{
+    public class RedisCacheResourceV2 : AzureResourceDefinitionV2
+    {
+        /// <summary>
+        /// The name of the redis cache.
+        /// </summary>
+        public string CacheName { get; set; }
+    }
+}
---
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v2/Providers/RedisCacheDeserializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model;
+using Promitor.Core.Scraping.Configuration.Serialization.v2.Model.ResourceTypes;
+using YamlDotNet.RepresentationModel;
+
+namespace Promitor.Core.Scraping.Configuration.Serialization.v2.Providers
+{
+    public class RedisCacheDeserializer : ResourceDeserializer
+    {
+        private const string CacheNameTag = "cacheName";
+
+        public RedisCacheDeserializer(ILogger logger) : base(logger)
+        {
+        }
+
+        protected override AzureResourceDefinitionV2 DeserializeResource(YamlMappingNode node)
+        {
+            return new RedisCacheResourceV2
+            {
+                CacheName = GetString(node, CacheNameTag)
+            };
+        }
+    }
+}
