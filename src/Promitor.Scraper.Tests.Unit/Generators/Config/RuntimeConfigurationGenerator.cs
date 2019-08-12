aff69690af6efe65178326de20a708bc8482ddbc
Tom Kerkhove <kerkhove.tom@gmail.com>
2019-08-12 19:18:22 +0200

    Remove redundant initializer
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Scraper.Tests.Unit/Generators/Config/RuntimeConfigurationGenerator.cs
+++ src/Promitor.Scraper.Tests.Unit/Generators/Config/RuntimeConfigurationGenerator.cs
@@ -17,7 +17,7 @@ namespace Promitor.Scraper.Tests.Unit.Generators.Config
     internal class RuntimeConfigurationGenerator
     {
         private readonly RuntimeConfiguration _runtimeConfiguration = new RuntimeConfiguration();
-        private bool? _enableMetricTimestampsInPrometheus = null;
+        private bool? _enableMetricTimestampsInPrometheus;
 
         private RuntimeConfigurationGenerator(ServerConfiguration serverConfiguration)
         {
