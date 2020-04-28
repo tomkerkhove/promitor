b5964ec2bee9f0b6521ac47c9110d4ee419693be
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-04-28 08:35:34 +0200

    Provide basics

---
+++ src/Promitor.Integrations.Sinks.Core/IMetricSink.cs
@@ -0,0 +1,9 @@
+﻿using Promitor.Core.Scraping;
+
+namespace Promitor.Integrations.Sinks.Core
+{
+    public interface IMetricSink
+    {
+        void ReportMetric(string metricName, string metricDescription, ScrapeResult scrapedMetricResult);
+    }
+}
---
+++ src/Promitor.Integrations.Sinks.Statsd/StatsdMetricSink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Promitor.Core.Scraping;
+using Promitor.Integrations.Sinks.Core;
+
+namespace Promitor.Integrations.Sinks.Statsd
+{
+    public class StatsdMetricSink : IMetricSink
+    {
+        public void ReportMetric(string metricName, string metricDescription, ScrapeResult scrapedMetricResult)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
