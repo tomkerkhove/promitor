47f7dc8283be8f48681e51be39d21e8a4591c0ac
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-05-24 20:15:24 +0200

    Add dependency tracking for resource discovery

--- src/Promitor.Agents.Scraper/Discovery/ResourceDiscoveryClient.cs
+++ src/Promitor.Agents.Scraper/Discovery/ResourceDiscoveryClient.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Arcus.Observability.Telemetry.Core;
 using GuardNet;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Promitor.Agents.Core.Serialization;
 
@@ -12,10 +14,14 @@ namespace Promitor.Agents.Scraper.Discovery
     public class ResourceDiscoveryClient
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public ResourceDiscoveryClient(IHttpClientFactory httpClientFactory)
-        {
-            Guard.NotNull(httpClientFactory,nameof(httpClientFactory));
+        private readonly ILogger<ResourceDiscoveryClient> _logger;
 
+        public ResourceDiscoveryClient(IHttpClientFactory httpClientFactory, ILogger<ResourceDiscoveryClient> logger)
+        {
+            Guard.NotNull(httpClientFactory, nameof(httpClientFactory));
+            Guard.NotNull(httpClientFactory, nameof(httpClientFactory));
+
+            _logger = logger;
             _httpClientFactory = httpClientFactory;
         }
 
@@ -36,14 +42,32 @@ namespace Promitor.Agents.Scraper.Discovery
 
         private async Task<string> SendGetRequestAsync(string uri)
         {
-            var client = _httpClientFactory.CreateClient("Promitor Resource Discovery");
-
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
-            var response = await client.SendAsync(request);
+
+            var response = await SendRequestToApiAsync(request);
             response.EnsureSuccessStatusCode();
 
             var rawResponse = await response.Content.ReadAsStringAsync();
             return rawResponse;
+        }
+
+        private async Task<HttpResponseMessage> SendRequestToApiAsync(HttpRequestMessage request)
+        {
+            var client = _httpClientFactory.CreateClient("Promitor Resource Discovery");
+            using (var dependencyMeasurement = DependencyMeasurement.Start())
+            {
+                HttpResponseMessage response = null;
+                try
+                {
+                    response = await client.SendAsync(request);
+                    return response;
+                }
+                finally
+                {
+                    var statusCode = response?.StatusCode ?? HttpStatusCode.InternalServerError;
+                    _logger.LogHttpDependency(request, statusCode, dependencyMeasurement);
+                }
+            }
         }
     }
 }
