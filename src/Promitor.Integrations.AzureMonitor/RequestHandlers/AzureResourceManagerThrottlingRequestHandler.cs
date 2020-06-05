9cf171219161774da949c8c5dee9d391860f6e62
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-06-05 18:57:07 +0200

    Code quality

--- src/Promitor.Integrations.AzureMonitor/RequestHandlers/AzureResourceManagerThrottlingRequestHandler.cs
+++ src/Promitor.Integrations.AzureMonitor/RequestHandlers/AzureResourceManagerThrottlingRequestHandler.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using GuardNet;
