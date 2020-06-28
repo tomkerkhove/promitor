93699f0d74cf5bf67d83baaed230804263bbc4ae
Adam Connelly <adam.rpconnelly@gmail.com>
2020-06-28 13:20:25 +0100

    fixup! fixup! Exit gracefully if a validation error occurs

--- src/Promitor.Agents.Scraper/Program.cs
+++ src/Promitor.Agents.Scraper/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Promitor.Agents.Core;
 using Promitor.Agents.Core.Configuration.Server;
 using Promitor.Agents.Scraper.Validation;
