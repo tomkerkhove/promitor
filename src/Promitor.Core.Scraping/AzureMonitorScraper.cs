39198f2173d450eb539e896eb07f725d8a9c6cb3
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-08-06 07:48:17 +0200

    WIP
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Core.Scraping/AzureMonitorScraper.cs
+++ src/Promitor.Core.Scraping/AzureMonitorScraper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Azure.Management.Monitor.Fluent.Models;
 using Promitor.Core.Contracts;
+using Promitor.Core.Metrics;
 using Promitor.Core.Scraping.Configuration.Model.Metrics;
 using MetricDimension = Promitor.Core.Scraping.Configuration.Model.MetricDimension;
 
@@ -26,16 +27,25 @@ namespace Promitor.Core.Scraping
         /// <inheritdoc />
         protected override async Task<ScrapeResult> ScrapeResourceAsync(string subscriptionId, ScrapeDefinition<IAzureResourceDefinition> scrapeDefinition, TResourceDefinition resourceDefinition, AggregationType aggregationType, TimeSpan aggregationInterval)
         {
+            // TODO: Docs on when to use this
             var resourceUri = BuildResourceUri(subscriptionId, scrapeDefinition, resourceDefinition);
 
             var metricFilter = DetermineMetricFilter(resourceDefinition);
             var metricName = scrapeDefinition.AzureMetricConfiguration.MetricName;
-            var dimensionName = DetermineMetricDimension(scrapeDefinition.AzureMetricConfiguration?.Dimension);
+            var dimensionName = DetermineMetricDimension(resourceDefinition, scrapeDefinition.AzureMetricConfiguration?.Dimension);
             var foundMetricValue = await AzureMonitorClient.QueryMetricAsync(metricName, dimensionName, aggregationType, aggregationInterval, resourceUri, metricFilter);
 
             var metricLabels = DetermineMetricLabels(resourceDefinition);
-            
-            return new ScrapeResult(subscriptionId, scrapeDefinition.ResourceGroupName, resourceDefinition.ResourceName, resourceUri, foundMetricValue, metricLabels);
+
+            var finalMetricValues = EnrichMeasuredMetrics(resourceDefinition, dimensionName, foundMetricValue);
+
+            return new ScrapeResult(subscriptionId, scrapeDefinition.ResourceGroupName, resourceDefinition.ResourceName, resourceUri, finalMetricValues, metricLabels);
+        }
+
+        // TODO: Docs on when to use this
+        protected virtual List<MeasuredMetric> EnrichMeasuredMetrics(TResourceDefinition resourceDefinition, string dimensionName, List<MeasuredMetric> metricValues)
+        {
+            return metricValues;
         }
 
         /// <summary>
@@ -51,7 +61,7 @@ namespace Promitor.Core.Scraping
         ///     Determines the dimension for a metric to use
         /// </summary>
         /// <param name="dimension">Provides information concerning the configured metric dimension.</param>
-        protected virtual string DetermineMetricDimension(MetricDimension dimension)
+        protected virtual string DetermineMetricDimension(TResourceDefinition resourceDefinition, MetricDimension dimension)
         {
             return dimension?.Name;
         }
