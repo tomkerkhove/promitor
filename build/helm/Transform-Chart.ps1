f802e520eba285d8b082022adca0bdee9be89983
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-08-06 09:01:33 +0200

    Replace image version in README
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- build/helm/Transform-Chart.ps1
+++ build/helm/Transform-Chart.ps1
@@ -7,26 +7,29 @@ Param(
     [Parameter(Mandatory = $true)] $imageTag = $(throw "Image tag is required")
 )
 
-echo 'Copying Chart folder'
+echo "Copying Chart folder"
 cp promitor-agent-scraper/ $chartName/ -r
 
-echo 'Determining image version'
+echo "Determining image version"
 $imageVersion = "$imageTag".ToLower()
-echo 'Image version is $imageVersion'
+echo "Image version is $imageVersion"
 
-echo 'Changing name of chart'
+echo "Changing name of chart to $chartName"
 ((Get-Content -path $chartName/Chart.yaml -Raw) -replace 'promitor-agent-scraper', $chartName) | Set-Content -Path $chartName/Chart.yaml
 
-echo 'Changing image tag'
+echo "Changing image tag to $imageVersion"
 ((Get-Content -path $chartName/values.yaml -Raw) -replace '1.0.0', $imageVersion) | Set-Content -Path $chartName/values.yaml
 
-echo 'Changing repo name'
+echo "Changing repo name to $imageName"
 ((Get-Content -path $chartName/values.yaml -Raw) -replace 'tomkerkhove/promitor-agent-scraper', $imageName) | Set-Content -Path $chartName/values.yaml
 
-echo 'Change name of chart in documentation samples'
-((Get-Content -path $chartName/README.md -Raw) -replace 'promitor-agent-scraper', 'promitor-agent-scraper-ci') | Set-Content -Path $chartName/README.md
+echo "Change name of chart in README to $chartName"
+((Get-Content -path $chartName/README.md -Raw) -replace 'promitor-agent-scraper', $chartName) | Set-Content -Path $chartName/README.md
 
-echo 'Outputting transformed content'
+echo "Change version of image in README to $imageVersion"
+((Get-Content -path $chartName/README.md -Raw) -replace '1.0.0', $imageVersion) | Set-Content -Path $chartName/README.md
+
+echo "Outputting transformed content"
 Get-Content -path $chartName/Chart.yaml -Raw
 Get-Content -path $chartName/values.yaml -Raw
 Get-Content -path $chartName/README.md -Raw
