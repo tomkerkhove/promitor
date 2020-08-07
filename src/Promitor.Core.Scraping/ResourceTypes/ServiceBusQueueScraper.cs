2d66a04ee2ec99c60ef166244e2201c2bacf858a
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-08-07 16:44:34 +0200

    Refactor 🎯
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Core.Scraping/ResourceTypes/ServiceBusQueueScraper.cs
+++ src/Promitor.Core.Scraping/ResourceTypes/ServiceBusQueueScraper.cs
@@ -1,17 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Logging;
-using Promitor.Core.Contracts;
+﻿using Promitor.Core.Contracts;
 using Promitor.Core.Contracts.ResourceTypes;
-using Promitor.Core.Metrics;
-using Promitor.Core.Scraping.Configuration.Model;
 using Promitor.Core.Scraping.Configuration.Model.Metrics;
 
 namespace Promitor.Core.Scraping.ResourceTypes
 {
-    public class ServiceBusQueueScraper : AzureMonitorScraper<ServiceBusQueueResourceDefinition>
+    public class ServiceBusQueueScraper : AzureMessagingScraper<ServiceBusQueueResourceDefinition>
     {
-        private const string EntityNameLabel = "entity_name";
         private const string ResourceUriTemplate = "subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.ServiceBus/namespaces/{2}";
 
         public ServiceBusQueueScraper(ScraperConfiguration scraperConfiguration)
@@ -24,56 +18,14 @@ namespace Promitor.Core.Scraping.ResourceTypes
             return string.Format(ResourceUriTemplate, subscriptionId, scrapeDefinition.ResourceGroupName, resource.Namespace);
         }
 
-        protected override List<MeasuredMetric> EnrichMeasuredMetrics(ServiceBusQueueResourceDefinition resourceDefinition, string dimensionName, List<MeasuredMetric> metricValues)
-        {
-            // Change Azure Monitor Dimension name to more representable value
-            foreach (var measuredMetric in metricValues.Where(metricValue => string.IsNullOrWhiteSpace(metricValue.DimensionName) == false))
-            {
-                measuredMetric.DimensionName = EntityNameLabel;
-            }
-
-            return metricValues;
-        }
-
-        protected override Dictionary<string, string> DetermineMetricLabels(ServiceBusQueueResourceDefinition resourceDefinition)
-        {
-            var metricLabels = base.DetermineMetricLabels(resourceDefinition);
-
-            if (IsQueueDeclared(resourceDefinition))
-            {
-                metricLabels.Add(EntityNameLabel, resourceDefinition.QueueName);
-            }
-
-            return metricLabels;
-        }
-
-        protected override string DetermineMetricDimension(ServiceBusQueueResourceDefinition resourceDefinition, MetricDimension dimension)
-        {
-            if (IsQueueDeclared(resourceDefinition))
-            {
-                return base.DetermineMetricDimension(resourceDefinition, dimension);
-            }
-
-            Logger.LogWarning("Using 'EntityName' dimension since no topic was configured.");
-
-            return "EntityName";
-        }
-
-        protected override string DetermineMetricFilter(ServiceBusQueueResourceDefinition resourceDefinition)
-        {
-            var entityName = "*";
-
-            if (IsQueueDeclared(resourceDefinition))
-            {
-                entityName = resourceDefinition.QueueName;
-            }
-
-            return $"EntityName eq '{entityName}'";
-        }
-
-        private static bool IsQueueDeclared(ServiceBusQueueResourceDefinition resourceDefinition)
+        protected override bool IsEntityDeclared(ServiceBusQueueResourceDefinition resourceDefinition)
         {
             return string.IsNullOrWhiteSpace(resourceDefinition.QueueName) == false;
+        }
+
+        protected override string GetEntityName(ServiceBusQueueResourceDefinition resourceDefinition)
+        {
+            return resourceDefinition.QueueName;
         }
     }
 }
