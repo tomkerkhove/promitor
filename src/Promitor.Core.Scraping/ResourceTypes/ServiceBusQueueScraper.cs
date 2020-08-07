03ff7714e7c5536ba56815c41eec81535eabccdc
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-08-07 16:15:25 +0200

    Provide support
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Core.Scraping/ResourceTypes/ServiceBusQueueScraper.cs
+++ src/Promitor.Core.Scraping/ResourceTypes/ServiceBusQueueScraper.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
 using Promitor.Core.Contracts;
 using Promitor.Core.Contracts.ResourceTypes;
+using Promitor.Core.Metrics;
+using Promitor.Core.Scraping.Configuration.Model;
 using Promitor.Core.Scraping.Configuration.Model.Metrics;
 
 namespace Promitor.Core.Scraping.ResourceTypes
 {
     public class ServiceBusQueueScraper : AzureMonitorScraper<ServiceBusQueueResourceDefinition>
     {
+        private const string EntityNameLabel = "entity_name";
         private const string ResourceUriTemplate = "subscriptions/{0}/resourceGroups/{1}/providers/Microsoft.ServiceBus/namespaces/{2}";
 
         public ServiceBusQueueScraper(ScraperConfiguration scraperConfiguration)
@@ -19,18 +24,56 @@ namespace Promitor.Core.Scraping.ResourceTypes
             return string.Format(ResourceUriTemplate, subscriptionId, scrapeDefinition.ResourceGroupName, resource.Namespace);
         }
 
+        protected override List<MeasuredMetric> EnrichMeasuredMetrics(ServiceBusQueueResourceDefinition resourceDefinition, string dimensionName, List<MeasuredMetric> metricValues)
+        {
+            // Change Azure Monitor Dimension name to more representable value
+            foreach (var measuredMetric in metricValues.Where(metricValue => string.IsNullOrWhiteSpace(metricValue.DimensionName) == false))
+            {
+                measuredMetric.DimensionName = EntityNameLabel;
+            }
+
+            return metricValues;
+        }
+
         protected override Dictionary<string, string> DetermineMetricLabels(ServiceBusQueueResourceDefinition resourceDefinition)
         {
             var metricLabels = base.DetermineMetricLabels(resourceDefinition);
 
-            metricLabels.TryAdd("entity_name", resourceDefinition.QueueName);
+            if (IsQueueDeclared(resourceDefinition))
+            {
+                metricLabels.Add(EntityNameLabel, resourceDefinition.QueueName);
+            }
 
             return metricLabels;
         }
 
+        protected override string DetermineMetricDimension(ServiceBusQueueResourceDefinition resourceDefinition, MetricDimension dimension)
+        {
+            if (IsQueueDeclared(resourceDefinition))
+            {
+                return base.DetermineMetricDimension(resourceDefinition, dimension);
+            }
+
+            Logger.LogWarning("Using 'EntityName' dimension since no topic was configured.");
+
+            return "EntityName";
+        }
+
         protected override string DetermineMetricFilter(ServiceBusQueueResourceDefinition resourceDefinition)
         {
-            return $"EntityName eq '{resourceDefinition.QueueName}'";
+            var entityName = "*";
+
+            if (IsQueueDeclared(resourceDefinition))
+            {
+                entityName = resourceDefinition.QueueName;
+            }
+
+            return $"EntityName eq '{entityName}'";
+        }
+
+        private static bool IsQueueDeclared(ServiceBusQueueResourceDefinition resourceDefinition)
+        {
+            return string.IsNullOrWhiteSpace(resourceDefinition.QueueName) == false;
         }
     }
 }
