26eb71871b5d016c2373f8e6e9d7b40ed1e2cc40
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-08-24 08:27:33 +0200

    Add basic step
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Promitor.Agents.ResourceDiscovery/Validation/Steps/ResourceDiscoveryGroupValidationStep.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Promitor.Agents.Core.Validation;
+using Promitor.Agents.Core.Validation.Interfaces;
+using Promitor.Agents.Core.Validation.Steps;
+using Promitor.Agents.ResourceDiscovery.Configuration;
+
+namespace Promitor.Agents.ResourceDiscovery.Validation.Steps
+{
+    public class ResourceDiscoveryGroupValidationStep : ValidationStep,
+        IValidationStep
+    {
+        private readonly IOptions<List<ResourceDiscoveryGroup>> _resourceDiscoveryGroups;
+
+        public ResourceDiscoveryGroupValidationStep(IOptions<List<ResourceDiscoveryGroup>> resourceDiscoveryGroups, ILogger<ResourceDiscoveryGroupValidationStep> logger) : base(logger)
+        {
+            _resourceDiscoveryGroups = resourceDiscoveryGroups;
+        }
+
+        public string ComponentName { get; } = "Resource Discovery Groups";
+
+        public ValidationResult Run()
+        {
+            return ValidationResult.Successful(ComponentName);
+        }
+    }
+}
