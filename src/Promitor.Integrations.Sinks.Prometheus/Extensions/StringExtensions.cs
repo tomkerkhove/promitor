d4234066b19f723a9fa56f2b3c9f559e46248685
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-08-31 14:55:03 +0200

    Sanitize label names to comply with Prometheus requirements (#1253)
    * Sanitize label names to comply with Prometheus requirements
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Add new metric
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Description
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Promitor.Integrations.Sinks.Prometheus/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿// ReSharper disable once CheckNamespace
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        ///     Ensures label key name is valid according to Prometheus requirements
+        /// </summary>
+        /// <param name="labelKey">Current label key value</param>
+        public static string SanitizeForPrometheusLabelKey(this string labelKey)
+        {
+            if (string.IsNullOrWhiteSpace(labelKey))
+            {
+                return labelKey;
+            }
+
+            return labelKey.Replace("/", "_").ToLower();
+        }
+    }
+}
---
+++ src/Promitor.Tests.Unit/Prometheus/StringExtensionTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using Xunit;
+
+namespace Promitor.Tests.Unit.Prometheus
+{
+    [Category("Unit")]
+    public class StringExtensionTests
+    {
+        [Fact]
+        public void SanitizeForPrometheusLabelKey_ValidKey_IdenticalOutput()
+        {
+            // Arrange
+            var labelKeyInput = "sample_label";
+
+            // Act
+            var sanitizedLabelKey = labelKeyInput.SanitizeForPrometheusLabelKey();
+
+            // Assert
+            Assert.Equal(labelKeyInput, sanitizedLabelKey);
+        }
+
+        [Fact]
+        public void SanitizeForPrometheusLabelKey_KeyIsUppercase_ConvertedToLowercase()
+        {
+            // Arrange
+            var labelKeyInput = "SAMPLE_LABEL";
+            var expectedLabelKey = "sample_label";
+
+            // Act
+            var sanitizedLabelKey = labelKeyInput.SanitizeForPrometheusLabelKey();
+
+            // Assert
+            Assert.Equal(expectedLabelKey, sanitizedLabelKey);
+        }
+
+        [Fact]
+        public void SanitizeForPrometheusLabelKey_KeyWithSlash_SlashIsReplaced()
+        {
+            // Arrange
+            var labelKeyInput = "sample/label";
+            var expectedLabelKey = "sample_label";
+
+            // Act
+            var sanitizedLabelKey = labelKeyInput.SanitizeForPrometheusLabelKey();
+
+            // Assert
+            Assert.Equal(expectedLabelKey, sanitizedLabelKey);
+        }
+    }
+}
