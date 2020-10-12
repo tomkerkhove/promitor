d912b965935591fa290c04692071bbb5a1a8996b
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-10-12 08:38:17 +0200

    Fix transformation
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- build/helm/Transform-Chart.ps1
+++ build/helm/Transform-Chart.ps1
@@ -17,16 +17,16 @@ $imageVersion = "$imageTag".ToLower()
 echo "Image version is $imageVersion"
 
 echo "Changing name of chart to $transformedChartName"
-((Get-Content -path $transformedChartName/Chart.yaml -Raw) -replace 'promitor-agent-scraper', $transformedChartName) | Set-Content -Path $transformedChartName/Chart.yaml
+((Get-Content -path $transformedChartName/Chart.yaml -Raw) -replace $chartName, $transformedChartName) | Set-Content -Path $transformedChartName/Chart.yaml
 
 echo "Changing image tag to $imageVersion"
 ((Get-Content -path $transformedChartName/values.yaml -Raw) -replace 'latest', $imageVersion) | Set-Content -Path $transformedChartName/values.yaml
 
 echo "Changing repo name to $imageName"
-((Get-Content -path $transformedChartName/values.yaml -Raw) -replace 'tomkerkhove/promitor-agent-scraper', $imageName) | Set-Content -Path $transformedChartName/values.yaml
+((Get-Content -path $transformedChartName/values.yaml -Raw) -replace 'tomkerkhove/' + $chartName, $imageName) | Set-Content -Path $transformedChartName/values.yaml
 
 echo "Change name of chart in README to $transformedChartName"
-((Get-Content -path $transformedChartName/README.md -Raw) -replace 'promitor-agent-scraper', $transformedChartName) | Set-Content -Path $transformedChartName/README.md
+((Get-Content -path $transformedChartName/README.md -Raw) -replace $chartName, $transformedChartName) | Set-Content -Path $transformedChartName/README.md
 
 echo "Change version of image in README to $imageVersion"
 ((Get-Content -path $transformedChartName/README.md -Raw) -replace 'latest', $imageVersion) | Set-Content -Path $transformedChartName/README.md
