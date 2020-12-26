e11255f870b40d9544091844e1efa0db7f04464b
Tom Kerkhove <kerkhove.tom@gmail.com>
2020-12-26 10:00:26 +0100

    Code quality improvement
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Agents.Scraper/Controllers/v1/HealthController.cs
+++ src/Promitor.Agents.Scraper/Controllers/v1/HealthController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
