3744638ec0ccf201828bbc495898db2392eafb6a
Ashwin <ashwin031192@gmail.com>
2021-10-02 22:15:38 +0530

    Formatting update

--- src/Promitor.Tests.Unit/Agents/ResourceDiscovery/AzureResourceRepositoryTests.cs
+++ src/Promitor.Tests.Unit/Agents/ResourceDiscovery/AzureResourceRepositoryTests.cs
@@ -65,22 +65,20 @@ namespace Promitor.Tests.Unit.Agents.ResourceDiscovery
         {
             return new List<AzureSubscriptionInformation> {
                  GetAzureSubscriptionInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "Visual Studio Enterprise",
-      "Enabled",
-      "Off",
-      "Sponsored_2016-01-01",
-      "RoleBased"),
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "Visual Studio Enterprise",
+                                                 "Enabled",
+                                                 "Off",
+                                                 "Sponsored_2016-01-01",
+                                                 "RoleBased"),
 
-                 GetAzureSubscriptionInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
-      "Windows Azure MSDN - Visual Studio Ultimate",
-      "Enabled",
-      "On",
-      "MSDN_2014-09-01",
-      "RoleBased"
-                     )
+                 GetAzureSubscriptionInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
+                                                 "Windows Azure MSDN - Visual Studio Ultimate",
+                                                 "Enabled",
+                                                 "On",
+                                                 "MSDN_2014-09-01",
+                                                 "RoleBased")
             };
         }
 
@@ -103,173 +101,137 @@ namespace Promitor.Tests.Unit.Agents.ResourceDiscovery
             return new List<AzureResourceGroupInformation>
             {
                 GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
-      "cds74642ORBE57UVKYYBQ255LLUAMD7R7KEFPG2YR5SNYAO7THHKAUB5BBFQ-North-Central-US",
-      "northcentralus",
-      "Succeeded",
-      ""),
+                                                 "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
+                                                 "cds74642ORBE57UVKYYBQ255LLUAMD7R7KEFPG2YR5SNYAO7THHKAUB5BBFQ-North-Central-US",
+                                                 "northcentralus",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
-      "promitor-sources",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
+                                                 "promitor-sources",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
-      "Default-Storage-WestEurope",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                  "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
+                                                  "Default-Storage-WestEurope",
+                                                  "westeurope",
+                                                  "Succeeded",
+                                                  ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
-      "promitor-testing-resource-discovery-us",
-      "westus2",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                  "0329dd2a-59dc-4493-aa54-cb01cb027dc2",
+                                                  "promitor-testing-resource-discovery-us",
+                                                  "westus2",
+                                                  "Succeeded",
+                                                  ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "azure-deprecation",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "azure-deprecation",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "promitor-sandbox",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "promitor-sandbox",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "demo-central-us",
-      "centralus",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "demo-central-us",
+                                                 "centralus",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "cloud-shell-storage-eastus",
-      "eastus",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "cloud-shell-storage-eastus",
+                                                 "eastus",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "promitor",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "promitor",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "NetworkWatcherRG",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "NetworkWatcherRG",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "promitor-sources",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "promitor-sources",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "keda-demos",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "keda-demos",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "promitor-testing-resource-discovery-eu",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "promitor-testing-resource-discovery-eu",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "DefaultResourceGroup-WEU",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "DefaultResourceGroup-WEU",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                     "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "demo",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "demo",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "azure-monitor-autoscale-to-event-grid-adapter",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "azure-monitor-autoscale-to-event-grid-adapter",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "MC_keda-demos_keda-demos_westeurope",
-      "westeurope",
-      "Succeeded",
-      "/subscriptions/0f9d7fea-99e8-4768-8672-06a28514f77e/resourcegroups/keda-demos/providers/Microsoft.ContainerService/managedClusters/keda-demos"
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "MC_keda-demos_keda-demos_westeurope",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 "/subscriptions/0f9d7fea-99e8-4768-8672-06a28514f77e/resourcegroups/keda-demos/providers/Microsoft.ContainerService/managedClusters/keda-demos"),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "aks-virtual-node",
-      "westeurope",
-      "Succeeded",
-      ""
-                    ),
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "aks-virtual-node",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 ""),
 
-                GetAzureResourceGroupInformation(
-                    "c8819874-9e56-4e3f-b1a8-1c0325138f27",
-      "0f9d7fea-99e8-4768-8672-06a28514f77e",
-      "virtual-node-preview",
-      "westeurope",
-      "Succeeded",
-      ""
-                    )
+                GetAzureResourceGroupInformation("c8819874-9e56-4e3f-b1a8-1c0325138f27",
+                                                 "0f9d7fea-99e8-4768-8672-06a28514f77e",
+                                                 "virtual-node-preview",
+                                                 "westeurope",
+                                                 "Succeeded",
+                                                 "")
             };
         }
 
