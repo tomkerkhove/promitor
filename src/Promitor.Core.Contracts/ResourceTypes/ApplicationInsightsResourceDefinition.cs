93f5d1f296fce278c304ff3a1ae68a36254a34ab
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-11-05 16:46:34 +0100

    Fix typo
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Core.Contracts/ResourceTypes/ApplicationInsightsResourceDefinition.cs
+++ src/Promitor.Core.Contracts/ResourceTypes/ApplicationInsightsResourceDefinition.cs
@@ -3,7 +3,7 @@ namespace Promitor.Core.Contracts.ResourceTypes
     public class ApplicationInsightsResourceDefinition : AzureResourceDefinition
     {
         public ApplicationInsightsResourceDefinition(string subscriptionId, string resourceGroupName, string applicationInsightsName)
-            : base(ResourceType.ApplicationInsights, subscriptionId, resourceGroupName, applicationGatewayName)
+            : base(ResourceType.ApplicationInsights, subscriptionId, resourceGroupName, applicationInsightsName)
         {
             Name = applicationInsightsName;
         }
