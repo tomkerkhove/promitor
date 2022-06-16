38f7ad80c85a4a4f4641869b57296b980eca18f5
Tom Kerkhove <kerkhove.tom@gmail.com>
2022-06-16 10:02:48 +0200

    Add new extension
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Promitor.Tests.Unit/Extensions/IDictionaryExtensions.cs
@@ -0,0 +1,14 @@
+﻿// ReSharper disable once CheckNamespace
+namespace System.Collections.Generic
+{
+    internal static class IDictionaryExtensions
+    {
+        public static void ForAll<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Action<KeyValuePair<TKey, TValue>> action)
+        {
+            foreach (var entry in dictionary)
+            {
+                action(entry);
+            }
+        }
+    }
+}
