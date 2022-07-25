2552ee784a182bffa41bc8da3c4c40102ebb66b5
Tom Kerkhove <kerkhove.tom@gmail.com>
2022-07-25 14:17:27 +0200

    Add dedicated test for OTEL running in pipeline on Linux only
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

---
+++ src/Promitor.Tests.Integration/Services/Scraper/MetricSinks/OpenTelemetryMetricSinkTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Promitor.Agents.Core;
+using Promitor.Tests.Integration.Clients;
+using Promitor.Tests.Integration.Data;
+using Promitor.Tests.Integration.Extensions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Promitor.Tests.Integration.Services.Scraper.MetricSinks
+{
+    [Trait("OS", "Linux")]
+    public class OpenTelemetryMetricSinkTests : ScraperIntegrationTest
+    {
+        public OpenTelemetryMetricSinkTests(ITestOutputHelper testOutput)
+          : base(testOutput)
+        {
+        }
+
+        [Fact]
+        public async Task OpenTelemetry_Scrape_ReturnsOk()
+        {
+            // Arrange
+            var scraperClient = new ScraperClient(Configuration, Logger);
+
+            // Act
+            var response = await scraperClient.ScrapeWithResponseAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("promitor_ratelimit_arm")]
+        [InlineData("promitor_scrape_success")]
+        [InlineData("promitor_scrape_error")]
+        [ClassData(typeof(AvailableMetricsTestInputGenerator))]
+        public async Task OpenTelemetry_Scrape_ExpectedMetricIsAvailable(string expectedMetricName)
+        {
+            // Arrange
+            var scraperClient = new ScraperClient(Configuration, Logger);
+
+            // Act
+            var gaugeMetric = await scraperClient.WaitForPrometheusMetricAsync(expectedMetricName);
+
+            // Assert
+            Assert.NotNull(gaugeMetric);
+            Assert.Equal(expectedMetricName, gaugeMetric.Name);
+            Assert.NotNull(gaugeMetric.Measurements);
+            Assert.False(gaugeMetric.Measurements.Count < 1);
+        }
+
+        [Theory]
+        [ClassData(typeof(AvailableMetricsTestInputGenerator))]
+        public async Task OpenTelemetry_Scrape_EveryMetricHasAnErrorMetric(string expectedMetricName)
+        {
+            // Arrange
+            const string errorMetricName = "promitor_scrape_error";
+            var scraperClient = new ScraperClient(Configuration, Logger);
+
+            // Act
+            var gaugeMetric = await scraperClient.WaitForPrometheusMetricAsync(errorMetricName, "metric_name", expectedMetricName);
+
+            // Assert
+            Assert.NotNull(gaugeMetric);
+        }
+
+        [Theory]
+        [ClassData(typeof(AvailableMetricsTestInputGenerator))]
+        public async Task OpenTelemetry_Scrape_EveryMetricHasAnSuccessMetric(string expectedMetricName)
+        {
+            // Arrange
+            const string errorMetricName = "promitor_scrape_success";
+            var scraperClient = new ScraperClient(Configuration, Logger);
+
+            // Act
+            var gaugeMetric = await scraperClient.WaitForPrometheusMetricAsync(errorMetricName, "metric_name", expectedMetricName);
+
+            // Assert
+            Assert.NotNull(gaugeMetric);
+        }
+    }
+}
