42bb6e974653b7c044a12e677d85ed2d98ecbb06
Tom Kerkhove <kerkhove.tom@gmail.com>
2022-08-11 15:17:54 +0200

    Revert
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Tests.Integration/Clients/PrometheusClient.cs
+++ src/Promitor.Tests.Integration/Clients/PrometheusClient.cs
@@ -108,9 +108,9 @@ namespace Promitor.Tests.Integration.Clients
             var context = new Dictionary<string, object>();
             try
             {
-                var rawResponse = await response.Content.ReadAsStringAsync();
+                await response.Content.ReadAsStringAsync();
                 // TODO: Uncomment for full payload during troubleshooting
-                context.Add("Body", rawResponse);
+                //context.Add("Body", rawResponse);
             }
             finally
             {
