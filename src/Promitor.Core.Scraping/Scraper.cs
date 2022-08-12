573554a8c9d0c78cb035187fb246b68c604c018f
Patrick Eichhorn <patrick.eichhorn@novatec-gmbh.de>
2022-08-12 16:09:55 +0200

    Change Log message

--- src/Promitor.Core.Scraping/Scraper.cs
+++ src/Promitor.Core.Scraping/Scraper.cs
@@ -142,7 +142,7 @@ namespace Promitor.Core.Scraping
             {
                 if (measuredMetric.IsDimensional)
                 {
-                    Logger.LogInformation("Found value {MetricValue} for metric {MetricName} with dimension {DimensionValue} as part of {DimensionName} dimension with aggregation interval {AggregationInterval}", measuredMetric.Value, scrapeDefinition.PrometheusMetricDefinition.Name, measuredMetric.DimensionValues, measuredMetric.DimensionNames, aggregationInterval);
+                    Logger.LogInformation("Found value {MetricValue} for metric {MetricName} with dimension values {DimensionValue} as part of the dimensions {DimensionName} with aggregation interval {AggregationInterval}", measuredMetric.Value, scrapeDefinition.PrometheusMetricDefinition.Name, measuredMetric.DimensionValues, measuredMetric.DimensionNames, aggregationInterval);
                 }
                 else
                 {
