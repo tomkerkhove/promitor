493dfd0ac480da3a0adb4310ab0f50518026ceb5
Ariel Haim <arielhaim@gmail.com>
2022-11-17 10:35:35 +0200

    Update src/Promitor.Integrations.Sinks.Statsd/Configuration/StatsdSinkConfiguration.cs
    Co-authored-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Integrations.Sinks.Statsd/Configuration/StatsdSinkConfiguration.cs
+++ src/Promitor.Integrations.Sinks.Statsd/Configuration/StatsdSinkConfiguration.cs
@@ -6,7 +6,6 @@
         public int Port { get; set; } = 8125;
         public string MetricPrefix { get; set; }
         public string MetricFormat { get; set; } = StatsdFormatterTypes.DEFAULT;
-        public string Namespace { get; set; }
-        public string Account { get; set; }
+        public GenevaConfiguration Geneva { get; set; }
     }
 }
