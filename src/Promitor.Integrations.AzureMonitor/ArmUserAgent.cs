3aed87fe5b2dfbf9769a946df74ea6e7665b12e4
Tom Kerkhove <kerkhove.tom@gmail.com>
2023-02-27 11:01:27 +0100

    feat: Improve user-agent for ARM with OS
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>

--- src/Promitor.Integrations.AzureMonitor/ArmUserAgent.cs
+++ src/Promitor.Integrations.AzureMonitor/ArmUserAgent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using GuardNet;
 using Promitor.Core;
 using Promitor.Core.Metrics.Sinks;
@@ -10,7 +11,7 @@ namespace Promitor.Integrations.AzureMonitor
     {
         private const string EnabledText = "Enabled";
         private const string DisabledText = "Disabled";
-        private const string ExtensionFormat = "{0} Prometheus/{1} OpenTelemetryCollector/{2} StatsD/{3} AtlassianStatuspage/{4}";
+        private const string ExtensionFormat = "{0} OS/{1} Prometheus/{2} OpenTelemetryCollector/{3} StatsD/{4} AtlassianStatuspage/{5}";
 
         public static string Generate(string agentVersion, List<MetricSinkType> metricSinkInfo)
         {
@@ -20,12 +21,13 @@ namespace Promitor.Integrations.AzureMonitor
             
             var originalUserAgent = UserAgent.Generate("Scraper", agentVersion);
 
+            var operatingSystem = RuntimeInformation.OSDescription.Contains("linux", StringComparison.InvariantCultureIgnoreCase) ? "Linux" : "Windows";
             var prometheusState = GetRawState(MetricSinkType.PrometheusScrapingEndpoint, metricSinkInfo);
             var openTelemetryState =  GetRawState(MetricSinkType.OpenTelemetryCollector, metricSinkInfo);
             var statsDState =  GetRawState(MetricSinkType.StatsD, metricSinkInfo);
             var statuspageState =  GetRawState(MetricSinkType.AtlassianStatuspage, metricSinkInfo);
 
-            return string.Format(ExtensionFormat, originalUserAgent, prometheusState, openTelemetryState, statsDState, statuspageState);
+            return string.Format(ExtensionFormat, originalUserAgent, operatingSystem, prometheusState, openTelemetryState, statsDState, statuspageState);
         }
 
         private static string GetRawState(MetricSinkType metricSinkType, List<MetricSinkType> enabledMetricSinks)
