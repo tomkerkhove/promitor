db4f397b9b16deaa0425734ce3b5daa347de1fc7
SurtzSean <55464380+SurtzSean@users.noreply.github.com>
2023-09-02 17:48:05 -0400

    Update PowerBiDedicatedDeserializer.cs

--- src/Promitor.Core.Scraping/Configuration/Serialization/v1/Providers/PowerBiDedicatedDeserializer.cs
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v1/Providers/PowerBiDedicatedDeserializer.cs
@@ -3,12 +3,12 @@ using Promitor.Core.Scraping.Configuration.Serialization.v1.Model.ResourceTypes;
 
 namespace Promitor.Core.Scraping.Configuration.Serialization.v1.Providers
 {
-	public class PowerBiDedicatedDeserializer : ResourceDeserializer<PowerBiDedicatedResourceV1>
+    public class PowerBiDedicatedDeserializer : ResourceDeserializer<PowerBiDedicatedResourceV1>
+    {
+        public PowerBiDedicatedDeserializer(ILogger<PowerBiDedicatedDeserializer> logger) : base(logger)
 	{
-		public PowerBiDedicatedDeserializer(ILogger<PowerBiDedicatedDeserializer> logger) : base(logger)
-		{
-            		Map(resource => resource.CapacityName)
-				.IsRequired();
-		}
+            Map(resource => resource.CapacityName)
+	        .IsRequired();
 	}
+    }
 }
