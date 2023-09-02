482c6675b487867c952ae17da86aeed92b4e59b5
SurtzSean <55464380+SurtzSean@users.noreply.github.com>
2023-09-02 17:46:29 -0400

    Update PowerBiDedicatedMetricValidator.cs

--- src/Promitor.Agents.Scraper/Validation/MetricDefinitions/ResourceTypes/PowerBiDedicatedMetricValidator.cs
+++ src/Promitor.Agents.Scraper/Validation/MetricDefinitions/ResourceTypes/PowerBiDedicatedMetricValidator.cs
@@ -7,18 +7,18 @@ using Promitor.Core.Contracts.ResourceTypes;
 
 namespace Promitor.Agents.Scraper.Validation.MetricDefinitions.ResourceTypes
 {
-	internal class PowerBiDedicatedMetricValidator : IMetricValidator
-	{
-		public IEnumerable<string> Validate(MetricDefinition metricDefinition)
-		{
-            		Guard.NotNull(metricDefinition, nameof(metricDefinition));
-            		foreach (var resourceDefinition in metricDefinition.Resources.Cast<PowerBiDedicatedResourceDefinition>())
-            		{
-                		if (string.IsNullOrWhiteSpace(resourceDefinition.CapacityName))
-                		{
-                    			yield return "No Power BI Dedicated capacity name is configured";
-                		}
-            		}
-        	}
-	}
+    internal class PowerBiDedicatedMetricValidator : IMetricValidator
+    {
+        public IEnumerable<string> Validate(MetricDefinition metricDefinition)
+        {
+            Guard.NotNull(metricDefinition, nameof(metricDefinition));
+            foreach (var resourceDefinition in metricDefinition.Resources.Cast<PowerBiDedicatedResourceDefinition>())
+            {
+                if (string.IsNullOrWhiteSpace(resourceDefinition.CapacityName))
+                {
+                    yield return "No Power BI Dedicated capacity name is configured";
+                }
+            }
+        }
+    }
 }
