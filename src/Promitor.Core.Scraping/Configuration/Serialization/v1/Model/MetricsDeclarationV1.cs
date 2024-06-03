0d3fe34bf57d4f95a8e00feb248d28cc5feb7c75
xchen <xchen@axon.com>
2024-06-02 18:52:51 -0700

    remove SDK flag out of metrics declaration

--- src/Promitor.Core.Scraping/Configuration/Serialization/v1/Model/MetricsDeclarationV1.cs
+++ src/Promitor.Core.Scraping/Configuration/Serialization/v1/Model/MetricsDeclarationV1.cs
@@ -9,7 +9,6 @@ namespace Promitor.Core.Scraping.Configuration.Serialization.v1.Model
     public class MetricsDeclarationV1
     {
         public string Version { get; set; } = SpecVersion.v1.ToString();
-        public bool UseAzureMonitorSdk { get; set; } = true;
         public AzureMetadataV1 AzureMetadata { get; set; }
         public MetricDefaultsV1 MetricDefaults { get; set; }
         public IReadOnlyCollection<MetricDefinitionV1> Metrics { get; set; }
