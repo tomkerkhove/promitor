b2cfd7959ffc2680bf3d7b012f1d5a89647534e2
xchen <xchen@axon.com>
2024-08-16 23:39:41 -0700

    log filter

--- src/Promitor.Integrations.AzureMonitor/Extensions/AzureMonitorQueryTasks.cs
+++ src/Promitor.Integrations.AzureMonitor/Extensions/AzureMonitorQueryTasks.cs
@@ -79,7 +79,7 @@ namespace Promitor.Integrations.AzureMonitor.Extensions
                     TimeRange= new QueryTimeRange(TimeSpan.FromHours(historyStartingFromInHours))
                 };
             }
-            logger.LogWarning("Batch query range: {Range}, size: {Size}, granularity: {Interval}, aggregation: {Aggregation}", queryOptions.TimeRange, querySizeLimit, metricInterval, queryOptions.Aggregations);
+            logger.LogWarning("Batch query range: {Range}, size: {Size}, granularity: {Interval}, aggregation: {Aggregation}, filter: {Filter}", queryOptions.TimeRange, queryOptions.Size, queryOptions.Granularity, queryOptions.Aggregations,  queryOptions.Filter);
             
             var metricsBatchQueryResponse = await metricsClient.QueryResourcesAsync(resourceIdentifiers, [metricName], metricNamespace, queryOptions);
             var metricsQueryResults = metricsBatchQueryResponse.Value;
