5b0b4cdcb3cfd51f2d375a273bde4ecc57053835
xchen <xchen@axon.com>
2024-09-09 22:53:03 -0700

    run single resource scraping for comparison

--- src/Promitor.Integrations.AzureMonitor/Configuration/AzureMonitorMetricBatchScrapeConfig.cs
+++ src/Promitor.Integrations.AzureMonitor/Configuration/AzureMonitorMetricBatchScrapeConfig.cs
@@ -6,6 +6,6 @@ namespace Promitor.Integrations.AzureMonitor.Configuration
     {
         public bool Enabled { get; set; }
         public int MaxBatchSize { get; set; }
-        public string AzureRegion { get; set; } // Batch scrape endpoints are deployed by region 
+        public string AzureRegion { get; set; } = "eastus"; // Batch scrape endpoints are deployed by region 
     }
 }
