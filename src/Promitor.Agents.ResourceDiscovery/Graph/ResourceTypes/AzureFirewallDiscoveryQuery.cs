3bfc5b18aad4e6770c5c6bdae8c85203c067d099
Loc Mai <locmai0201@gmail.com>
2024-09-12 14:04:19 +0700

    add resource query and validator
    Signed-off-by: Loc Mai <locmai0201@gmail.com>

---
+++ src/Promitor.Agents.ResourceDiscovery/Graph/ResourceTypes/AzureFirewallDiscoveryQuery.cs
@@ -0,0 +1,23 @@
+﻿using GuardNet;
+using Newtonsoft.Json.Linq;
+using Promitor.Core.Contracts;
+using Promitor.Core.Contracts.ResourceTypes;
+
+namespace Promitor.Agents.ResourceDiscovery.Graph.ResourceTypes
+{
+    public class AzureFirewallDiscoveryQuery : ResourceDiscoveryQuery
+    {
+        public override string[] ResourceTypes => new[] { "microsoft.network/azureFirewalls" };
+        public override string[] ProjectedFieldNames => new[] { "subscriptionId", "resourceGroup", "type", "name" };
+
+        public override AzureResourceDefinition ParseResults(JToken resultRowEntry)
+        {
+            Guard.NotNull(resultRowEntry, nameof(resultRowEntry));
+
+            var azureFirewallName = resultRowEntry[3]?.ToString();
+
+            var resource = new AzureFirewallResourceDefinition(resultRowEntry[0]?.ToString(), resultRowEntry[1]?.ToString(), azureFirewallName);
+            return resource;
+        }
+    }
+}
