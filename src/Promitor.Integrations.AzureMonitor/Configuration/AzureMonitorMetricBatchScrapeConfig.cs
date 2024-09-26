457330098113d15ee1812bb452db10fc4a161ee8
xchen <xchen@axon.com>
2024-09-26 00:11:55 -0700

    add null check

--- src/Promitor.Integrations.AzureMonitor/Configuration/AzureMonitorMetricBatchScrapeConfig.cs
+++ src/Promitor.Integrations.AzureMonitor/Configuration/AzureMonitorMetricBatchScrapeConfig.cs
@@ -4,7 +4,7 @@ namespace Promitor.Integrations.AzureMonitor.Configuration
 {
     public class AzureMonitorMetricBatchScrapeConfig
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Explicit initialization to false for better readability")]
+        [SuppressMessage("ReSharper", "RedundantDefaultMemberInitializer", Justification = "Explicit initialization to false for better readability")]
         public bool Enabled { get; set; } = false; 
         public int MaxBatchSize { get; set; }
         public string AzureRegion { get; set; } // Batch scrape endpoints are deployed by region 
