6c2652c13cb32d5d4f1af5c80df810d0c26801a6
xchen <xchen@axon.com>
2025-03-13 13:25:13 -0700

    busy wait after first cycle(experimentation)

--- src/Promitor.Agents.Scraper/Scheduling/ResourcesScrapingJob.cs
+++ src/Promitor.Agents.Scraper/Scheduling/ResourcesScrapingJob.cs
@@ -308,12 +308,15 @@ namespace Promitor.Agents.Scraper.Scheduling
         private async Task ScrapeMetricBatched(BatchScrapeDefinition<IAzureResourceDefinition> batchScrapeDefinition, int? runNum) {
             try
             {
-                while (true) 
+                if (runNum > 1)
                 {
-                    Logger.LogWarning("Run number {runNum}", runNum.ToString());
-                    await Task.Delay(10000);
+                    while (true) 
+                    {
+                        Logger.LogWarning("Run number {runNum}", runNum.ToString());
+                        await Task.Delay(10000);
+                    }
                 }
-
+                
                 var resourceSubscriptionId = batchScrapeDefinition.ScrapeDefinitionBatchProperties.SubscriptionId;
                 var azureMonitorClient = _azureMonitorClientFactory.CreateIfNotExists(_metricsDeclaration.AzureMetadata, _metricsDeclaration.AzureMetadata.TenantId,
                     resourceSubscriptionId, _metricSinkWriter, _azureScrapingSystemMetricsPublisher, _resourceMetricDefinitionMemoryCache, _configuration,
